8e9cb05c0777eda45414167e1c56de8fb41f1cd9
thomas <thomasmonta98@gmail.com>
2021-09-08 22:37:13 +1000

    done with assignment

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select{ |meal| meal.waiter == self }
+  end
+
+  def best_tipper
+    best_tipper = self.meals[0]
+    self.meals.each do |customer|
+      best_tipper = customer if customer.tip > best_tipper.tip
+    end
+    best_tipper.customer
+  end
+
+  def self.all
+    @@all
+  end
+
 end
